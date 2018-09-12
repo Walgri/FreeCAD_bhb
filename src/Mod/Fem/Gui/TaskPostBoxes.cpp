5e2871affd45e6eeb880d2814d91d65e26c239c4
Bernd Hahnebach <bernd@bimstatik.org>
2018-09-12 07:10:55 +0200

    Glyph improvements in task panel

--- src/Mod/Fem/Gui/TaskPostBoxes.cpp
+++ src/Mod/Fem/Gui/TaskPostBoxes.cpp
@@ -1265,6 +1265,9 @@ void TaskPostCut::on_FunctionBox_currentIndexChanged(int idx) {
 }
 
 //############################################################################################
+// glyph filter
+// spinbox min, slider, spinbox max
+// spinbox scale factor
 
 TaskPostGlyph::TaskPostGlyph(ViewProviderDocumentObject* view, QWidget* parent) :
     TaskPostBox(view, Gui::BitmapFactory().pixmap("fem-femmesh-create-node-by-poly"), tr("Glyph options"), parent) {
