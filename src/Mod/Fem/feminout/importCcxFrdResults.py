91e12af16a6deb21df4c500a58e96baca10a1c2c
HarryvL <harryvanlangen.doha@gmail.com>
2018-12-31 08:12:13 +0100

    FEM: concrete, some dev for better analysis of concrete structures

--- src/Mod/Fem/feminout/importCcxFrdResults.py
+++ src/Mod/Fem/feminout/importCcxFrdResults.py
@@ -105,7 +105,9 @@ def importFrd(filename, analysis=None, result_name_prefix=None):
                     res_obj = restools.compact_result(res_obj)
                 res_obj = restools.add_disp_apps(res_obj)  # fill DisplacementLengths
                 res_obj = restools.add_von_mises(res_obj)  # fill StressValues
-                res_obj = restools.add_principal_stress(res_obj)  # fill PrincipalMax, PrincipalMed, PrincipalMin, MaxShear
+                # temporary deacitvate this because they are calculated during result object data filling
+                # TODO move all this new code from Harry into result tools
+                # res_obj = restools.add_principal_stress(res_obj)  # fill PrincipalMax, PrincipalMed, PrincipalMin, MaxShear
                 res_obj = restools.fill_femresult_stats(res_obj)  # fill Stats
         else:
             error_message = (
